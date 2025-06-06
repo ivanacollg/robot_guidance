5bd37c69b9eefceba7244136bec065eb776155e5
MHCthegreat532 <mhcthegreat@icloud.com>
2025-06-06 13:38:04 -0400

    cancel func and client status to  depth_control_client

--- robot_guidance_pkg/robot_guidance_pkg/depth_control_client.py
+++ robot_guidance_pkg/robot_guidance_pkg/depth_control_client.py
@@ -1,7 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.action import ActionClient
-from rclpy.action.client import ClientGoalHandle
+from rclpy.action.client import ClientGoalHandle, GoalStatus
 from robot_guidance_interfaces.action import GoToDepth
 
 class DepthControlClient(Node):
@@ -24,6 +24,8 @@ class DepthControlClient(Node):
             send_goal_async(goal, feedback_callback=self.goal_feedback_callback). \
                 add_done_callback(self.goal_response_callback)
 
+        # timer to cancel goal to test functionality
+        # self.timer_ = self.create_timer(10.0, self.cancel_goal)
 
     def goal_response_callback(self, future):
         self.goal_handle_:ClientGoalHandle = future.result()
@@ -34,13 +36,30 @@ class DepthControlClient(Node):
 
 
     def goal_result_callback(self, future):
+        status = future.result().status
         result = future.result().result
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info("Success")
+        elif status == GoalStatus.STATUS_ABORTED:
+            self.get_logger().error("Aborted")
+        elif status == GoalStatus.STATUS_CANCELED:
+            self.get_logger().warn("Canceled")
         self.get_logger().info("Result: " + str(result.success))
 
     
     def goal_feedback_callback(self, feedback_msg):
         current_depth = feedback_msg.feedback.current_depth
-        self.get_logger().info("Got feedback: " + str(current_depth))
+        self.get_logger().info("Current depth: " + str(current_depth))
+
+
+    def cancel_goal(self):
+        self.get_logger().info("Send a cancel request")
+        self.goal_handle_.cancel_goal_async()
+        
+        # timer to test functionality
+       # self.timer_.cancel()
+
+
 
 def main(args=None):
     rclpy.init(args=args)
