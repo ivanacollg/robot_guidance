4b8611b6b8b416727531599d15d987c422c8db5c
MHCthegreat532 <mhcthegreat@icloud.com>
2025-06-11 12:11:08 -0400

    depth_controller_launch with velocity integrator and lifecycle manager

--- robot_guidance_pkg/launch/depth_controller_launch.py
+++ robot_guidance_pkg/launch/depth_controller_launch.py
@@ -1,7 +1,9 @@
 import os
 
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -13,7 +15,14 @@ def generate_launch_description():
     pkg_path = get_package_share_directory('robot_guidance_pkg')
     param_file = os.path.join(pkg_path, 'config', 'depth_control_params.yaml')
 
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation time (true for Gazebo or fake clock)'
+        ),
 
         Node(
             package='robot_guidance_pkg',
@@ -27,5 +36,31 @@ def generate_launch_description():
                 }
             ],
             env=env
-        )
+        ),
+
+        Node(
+            package='robot_guidance_pkg',
+            executable='velocity_integrator',
+            name='amcl',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'cmd_vel_topic': '/cmd_vel',
+                'odom_topic': '/odom',
+            }]
+        ),
+
+        # Lifecycle manager
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_navigation',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'autostart': True,
+                'bond_timeout': 0.0, # Fix to allow velocity integrator localization without bonding
+                'node_names': ['amcl']
+            }]
+        ),
     ])
